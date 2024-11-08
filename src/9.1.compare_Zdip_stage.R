7bb153fefff82ded1a198dac2f95a78d34230da4
Nirwan Tandukar <tandukar.nirwan@gmail.com>
2024-11-08 11:55:52 -0500

    update

--- src/9.1.compare_Zdip_stage.R
+++ src/9.1.compare_Zdip_stage.R
@@ -282,7 +282,18 @@ all_control_med_VT <- rbind(predicted_N_zdip_med_VT_e1_C,
                             predicted_N_zdip_med_VT_e3_BD,
                             predicted_N_zdip_med_VT_e3_BD1,
                             predicted_N_zdip_med_VT_e3_BD2)
+str(all_control_med_VT)
+unique(all_control_med_VT$plant)
+# Replace "C1" and "C2" with "C1_C2", and "BD1" and "BD2" with "BD1_BD2"
+all_control_med_VT <- all_control_med_VT %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("C1", "C2") ~ "C1_C2",
+    plant %in% c("BD1", "BD2") ~ "BD1_BD2",
+    TRUE ~ plant
+  ))
 
+# Print unique values to confirm the changes
+unique(all_control_med_VT$plant)
 
 all_control_med_R1 <- rbind(predicted_N_zdip_med_R1_e1_C,
                             predicted_N_zdip_med_R1_e1_C1,
@@ -302,6 +313,14 @@ all_control_med_R1 <- rbind(predicted_N_zdip_med_R1_e1_C,
                             predicted_N_zdip_med_R1_e3_BD,
                             predicted_N_zdip_med_R1_e3_BD1,
                             predicted_N_zdip_med_R1_e3_BD2)
+# Replace "C1" and "C2" with "C1_C2", and "BD1" and "BD2" with "BD1_BD2"
+all_control_med_R1 <- all_control_med_R1 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("C1", "C2") ~ "C1_C2",
+    plant %in% c("BD1", "BD2") ~ "BD1_BD2",
+    TRUE ~ plant
+  ))
+
 
 all_control_med_R4 <- rbind(predicted_N_zdip_med_R4_e1_C,
                             predicted_N_zdip_med_R4_e1_C1,
@@ -321,10 +340,13 @@ all_control_med_R4 <- rbind(predicted_N_zdip_med_R4_e1_C,
                             predicted_N_zdip_med_R4_e3_BD,
                             predicted_N_zdip_med_R4_e3_BD1,
                             predicted_N_zdip_med_R4_e3_BD2)
-
-all_desinked_med_R4 <- rbind(predicted_N_zdip_med_R4_e1_AD1,
-                             predicted_N_zdip_med_R4_e1_AD2,
-                             predicted_N_zdip_med_R4_e1_AD)
+# Replace "C1" and "C2" with "C1_C2", and "BD1" and "BD2" with "BD1_BD2"
+all_control_med_R4 <- all_control_med_R4 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("C1", "C2") ~ "C1_C2",
+    plant %in% c("BD1", "BD2") ~ "BD1_BD2",
+    TRUE ~ plant
+  ))
 
 
 all_control_high_VT <- rbind(predicted_N_zdip_high_VT_e1_C,
@@ -345,6 +367,13 @@ all_control_high_VT <- rbind(predicted_N_zdip_high_VT_e1_C,
                              predicted_N_zdip_high_VT_e3_BD,
                              predicted_N_zdip_high_VT_e3_BD1,
                              predicted_N_zdip_high_VT_e3_BD2)
+# Replace "C1" and "C2" with "C1_C2", and "BD1" and "BD2" with "BD1_BD2"
+all_control_high_VT <- all_control_high_VT %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("C1", "C2") ~ "C1_C2",
+    plant %in% c("BD1", "BD2") ~ "BD1_BD2",
+    TRUE ~ plant
+  ))
 
 
 all_control_high_R1 <- rbind(predicted_N_zdip_high_R1_e1_C,
@@ -365,6 +394,13 @@ all_control_high_R1 <- rbind(predicted_N_zdip_high_R1_e1_C,
                              predicted_N_zdip_high_R1_e3_BD,
                              predicted_N_zdip_high_R1_e3_BD1,
                              predicted_N_zdip_high_R1_e3_BD2)
+# Replace "C1" and "C2" with "C1_C2", and "BD1" and "BD2" with "BD1_BD2"
+all_control_high_R1 <- all_control_high_R1 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("C1", "C2") ~ "C1_C2",
+    plant %in% c("BD1", "BD2") ~ "BD1_BD2",
+    TRUE ~ plant
+  ))
 
 
 
@@ -386,7 +422,35 @@ all_control_high_R4 <- rbind(predicted_N_zdip_high_R4_e1_C,
                              predicted_N_zdip_high_R4_e3_BD,
                              predicted_N_zdip_high_R4_e3_BD1,
                              predicted_N_zdip_high_R4_e3_BD2)
+# Replace "C1" and "C2" with "C1_C2", and "BD1" and "BD2" with "BD1_BD2"
+all_control_high_R4 <- all_control_high_R4 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("C1", "C2") ~ "C1_C2",
+    plant %in% c("BD1", "BD2") ~ "BD1_BD2",
+    TRUE ~ plant
+  ))
 
+
+########## DESINKED
+all_desinked_med_VT <- rbind(predicted_N_zdip_med_VT_e1_AD1,
+                             predicted_N_zdip_med_VT_e1_AD2,
+                             predicted_N_zdip_med_VT_e1_AD,
+                             predicted_N_zdip_med_VT_e2_AD1,
+                             predicted_N_zdip_med_VT_e2_AD2,
+                             predicted_N_zdip_med_VT_e2_AD,
+                             predicted_N_zdip_med_VT_e3_AD1,
+                             predicted_N_zdip_med_VT_e3_AD2,
+                             predicted_N_zdip_med_VT_e3_AD)
+
+all_desinked_high_VT <- rbind(predicted_N_zdip_high_VT_e1_AD1,
+                              predicted_N_zdip_high_VT_e1_AD2,
+                              predicted_N_zdip_high_VT_e1_AD,
+                              predicted_N_zdip_high_VT_e2_AD1,
+                              predicted_N_zdip_high_VT_e2_AD2,
+                              predicted_N_zdip_high_VT_e2_AD,
+                              predicted_N_zdip_high_VT_e3_AD1,
+                              predicted_N_zdip_high_VT_e3_AD2,
+                              predicted_N_zdip_high_VT_e3_AD)
 
 
 
@@ -399,6 +463,14 @@ all_desinked_med_R1 <- rbind(predicted_N_zdip_med_R1_e1_AD1,
                              predicted_N_zdip_med_R1_e3_AD1,
                              predicted_N_zdip_med_R1_e3_AD2,
                              predicted_N_zdip_med_R1_e3_AD)
+# Replace "A1" and "A2" with "A1_A2"
+all_desinked_med_R1 <- all_desinked_med_R1 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("AD1", "AD2") ~ "AD1_AD2",
+    TRUE ~ plant
+  ))
+
+
 
 all_desinked_high_R1 <- rbind(predicted_N_zdip_high_R1_e1_AD1,
                               predicted_N_zdip_high_R1_e1_AD2,
@@ -409,6 +481,12 @@ all_desinked_high_R1 <- rbind(predicted_N_zdip_high_R1_e1_AD1,
                               predicted_N_zdip_high_R1_e3_AD1,
                               predicted_N_zdip_high_R1_e3_AD2,
                               predicted_N_zdip_high_R1_e3_AD)
+# Replace "A1" and "A2" with "A1_A2"
+all_desinked_high_R1 <- all_desinked_high_R1 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("AD1", "AD2") ~ "AD1_AD2",
+    TRUE ~ plant
+  ))
 
 all_desinked_med_R4 <- rbind(predicted_N_zdip_med_R4_e1_AD1,
                              predicted_N_zdip_med_R4_e1_AD2,
@@ -419,6 +497,12 @@ all_desinked_med_R4 <- rbind(predicted_N_zdip_med_R4_e1_AD1,
                              predicted_N_zdip_med_R4_e3_AD1,
                              predicted_N_zdip_med_R4_e3_AD2,
                              predicted_N_zdip_med_R4_e3_AD)
+# Replace "A1" and "A2" with "A1_A2"
+all_desinked_med_R4 <- all_desinked_med_R4 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("AD1", "AD2") ~ "AD1_AD2",
+    TRUE ~ plant
+  ))
 
 all_desinked_high_R4 <- rbind(predicted_N_zdip_high_R4_e1_AD1,
                               predicted_N_zdip_high_R4_e1_AD2,
@@ -429,26 +513,88 @@ all_desinked_high_R4 <- rbind(predicted_N_zdip_high_R4_e1_AD1,
                               predicted_N_zdip_high_R4_e3_AD1,
                               predicted_N_zdip_high_R4_e3_AD2,
                               predicted_N_zdip_high_R4_e3_AD)
+# Replace "A1" and "A2" with "A1_A2"
+all_desinked_high_R4 <- all_desinked_high_R4 %>%
+  dplyr::mutate(plant = dplyr::case_when(
+    plant %in% c("AD1", "A2") ~ "AD1_A2",
+    TRUE ~ plant
+  ))
 
 
 # Not good data:
 # Remove 454 from plot
 all_control_high_R4 <- all_control_high_R4[all_control_high_R4$old_genotype != 454, ]
 str(all_control_high_R4)
+unique(all_control_high_R4$plant)
+unique(all_control_high_R4$leaf)
 
 
 
-##### looping through it all
 # List of stages and treatments to loop through
 stages <- c("VT", "R1", "R4")
 treatments <- c("med", "high")
 
 # Loop through each stage and treatment combination
-# Load necessary libraries
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+for (stage in stages) {
+  for (treatment in treatments) {
+    # Construct the variable name for the dataset
+    dataset_name <- paste0("all_control_", treatment, "_", stage)
+    
+    # Use get() to dynamically access the dataset
+    data_filtered <- get(dataset_name)
+    
+    # Calculate the average predicted_N by old_genotype, plant, and leaf
+    genotype_plant_summary <- data_filtered %>%
+      group_by(old_genotype, plant, leaf) %>%
+      summarise(
+        mean_predicted_N = mean(predicted_N, na.rm = TRUE),
+        .groups = 'drop'
+      )
+    
+    # Print the summary for verification
+    print(genotype_plant_summary)
+    
+    # Save the summary as a CSV file
+    output_filename <- paste0("genotype_plant_summary_Control_", treatment, "_", stage, ".csv")
+    write.csv(genotype_plant_summary, output_filename, row.names = FALSE)
+  }
+}
 
+
+# For De-sinked
+# Loop through each stage and treatment combination
+for (stage in stages) {
+  for (treatment in treatments) {
+    # Construct the variable name for the dataset
+    dataset_name <- paste0("all_desinked_", treatment, "_", stage)
+    
+    # Use get() to dynamically access the dataset
+    data_filtered <- get(dataset_name)
+    
+    # Calculate the average predicted_N by old_genotype, plant, and leaf
+    genotype_plant_summary <- data_filtered %>%
+      group_by(old_genotype, plant, leaf) %>%
+      summarise(
+        mean_predicted_N = mean(predicted_N, na.rm = TRUE),
+        .groups = 'drop'
+      )
+    
+    # Print the summary for verification
+    print(genotype_plant_summary)
+    
+    # Save the summary as a CSV file
+    output_filename <- paste0("genotype_plant_summary_desinked_", treatment, "_", stage, ".csv")
+    write.csv(genotype_plant_summary, output_filename, row.names = FALSE)
+  }
+}
+
+
+
+# Save the summary as a CSV file
+#write.csv(genotype_plant_summary_high_R4, "genotype_plant_summary_high_R4.csv", row.names = FALSE)
+
+
+######## SUMMARIZING THE DATA
 # List of stages and treatments to loop through
 stages <- c("VT", "R1", "R4")
 treatments <- c("med", "high")
@@ -458,127 +604,28 @@ for (stage in stages) {
   for (treatment in treatments) {
     # Filter the data for the current stage and treatment
     data_filtered <- all_control_med %>%
-      dplyr::filter(stage == stage, N_treatment == treatment)
+      filter(stage == stage, N_treatment == treatment)
     
-    # Calculate mean and 95% CI for B73 x LH287
-    b73_summary <- data_filtered %>%
-      dplyr::filter(old_genotype == "B73 x LH287") %>%
-      dplyr::group_by(leaf) %>%
-      dplyr::summarise(
-        mean_predicted_N_B73 = mean(predicted_N, na.rm = TRUE),
-        lower_CI = mean_predicted_N_B73 - qt(0.975, df = n() - 1) * (sd(predicted_N, na.rm = TRUE) / sqrt(n())),
-        upper_CI = mean_predicted_N_B73 + qt(0.975, df = n() - 1) * (sd(predicted_N, na.rm = TRUE) / sqrt(n()))
-      )
-    
-    # Calculate mean predicted N for the remaining genotypes
-    averaged_genotypes <- data_filtered %>%
-      dplyr::filter(old_genotype != "B73 x LH287") %>%
-      dplyr::group_by(old_genotype, leaf) %>%
-      dplyr::summarise(
+    # Calculate the average based on plant and leaf
+    plant_summary <- data_filtered %>%
+      group_by(plant, leaf) %>%
+      summarise(
         mean_predicted_N = mean(predicted_N, na.rm = TRUE),
-        plant = paste(unique(plant), collapse = ", "),
         .groups = 'drop'
       )
     
-    # Save averaged genotypes data
-    write.csv(averaged_genotypes, paste0("averaged_genotypes_", treatment, "_control_", stage, ".csv"))
+    # Print the summary for each combination
+    print(paste("Summary for stage:", stage, "and treatment:", treatment))
+    print(plant_summary)
     
-    # Identify top 25 most divergent genotypes
-    different_genotypes_ranked <- averaged_genotypes %>%
-      dplyr::inner_join(b73_summary, by = "leaf") %>%
-      dplyr::mutate(abs_diff = abs(mean_predicted_N - mean_predicted_N_B73)) %>%
-      dplyr::group_by(old_genotype) %>%
-      dplyr::summarise(total_abs_diff = sum(abs_diff, na.rm = TRUE)) %>%
-      dplyr::arrange(desc(total_abs_diff))
-    
-    top_25_genotypes <- different_genotypes_ranked %>%
-      dplyr::slice_head(n = 25)
-    
-    # Subset top 25 genotypes
-    top_25_genotypes <- averaged_genotypes %>%
-      dplyr::filter(old_genotype %in% top_25_genotypes$old_genotype)
-    
-    # Plot B73 and top 25 genotypes
-    b73_plot <- ggplot2::ggplot(b73_summary, aes(x = leaf, y = mean_predicted_N_B73, group = 1)) +
-      ggplot2::geom_line(color = "blue", size = 1.2) +
-      ggplot2::geom_ribbon(aes(ymin = lower_CI, ymax = upper_CI), fill = "lightblue", alpha = 0.3) +
-      ggplot2::geom_point(color = "blue", size = 2) +
-      ggplot2::labs(
-        title = paste("95% Confidence Interval for B73 x LH287 with Zdip Genotypes (", stage, " Stage) for", treatment, "Nitrogen"),
-        x = "Leaf Stage",
-        y = "Predicted N (%)"
-      ) +
-      ggplot2::theme_minimal(base_size = 15) +
-      ggplot2::theme(
-        panel.grid = ggplot2::element_blank(),
-        panel.border = ggplot2::element_rect(color = "black", fill = NA)
-      )
-    
-    # Add lines for top 25 genotypes
-    final_plot <- b73_plot +
-      ggplot2::geom_line(data = top_25_genotypes, aes(x = leaf, y = mean_predicted_N, group = old_genotype, color = old_genotype), size = 1) +
-      ggplot2::geom_point(data = top_25_genotypes, aes(x = leaf, y = mean_predicted_N, color = old_genotype), size = 2) +
-      ggplot2::scale_color_manual(values = genotype_colors) +
-      ggplot2::labs(color = "Genotype")
-    
-    # Save the plot
-    ggplot2::ggsave(
-      filename = paste0("Top25_divergent_genotype_", treatment, "_", stage, ".png"),
-      plot = final_plot,
-      device = "png",
-      width = 14,
-      height = 6,
-      units = "in",
-      dpi = 300,
-      bg = "white"
-    )
-    
-    # Identify genotypes above and below CI, and other conditions
-    all_above_CI <- averaged_genotypes %>%
-      dplyr::inner_join(b73_summary, by = "leaf") %>%
-      dplyr::filter(mean_predicted_N > upper_CI) %>%
-      dplyr::group_by(old_genotype) %>%
-      dplyr::summarise(count_above_CI = n()) %>%
-      dplyr::filter(count_above_CI == 3)
-    
-    all_above_CI_details <- averaged_genotypes %>%
-      dplyr::filter(old_genotype %in% all_above_CI$old_genotype)
-    
-    all_above_CI_details$condition <- paste0("all_above_CI_", treatment, "_", stage)
-    
-    all_below_CI <- averaged_genotypes %>%
-      dplyr::inner_join(b73_summary, by = "leaf") %>%
-      dplyr::filter(mean_predicted_N < lower_CI) %>%
-      dplyr::group_by(old_genotype) %>%
-      dplyr::summarise(count_below_CI = n()) %>%
-      dplyr::filter(count_below_CI == 3)
-    
-    all_below_CI_details <- averaged_genotypes %>%
-      dplyr::filter(old_genotype %in% all_below_CI$old_genotype)
-    
-    all_below_CI_details$condition <- paste0("all_below_CI_", treatment, "_", stage)
-    
-    e3_lower_genotypes <- averaged_genotypes %>%
-      tidyr::pivot_wider(names_from = leaf, values_from = mean_predicted_N) %>%
-      dplyr::filter(e3 < e1 - 0.25 & e3 < e2 - 0.25) %>%
-      tidyr::pivot_longer(cols = c(e1, e2, e3), names_to = "leaf", values_to = "mean_predicted_N")
-    
-    e3_lower_genotypes$condition <- paste0("e3_lowest_", treatment, "_", stage)
-    
-    e3_higher_genotypes <- averaged_genotypes %>%
-      tidyr::pivot_wider(names_from = leaf, values_from = mean_predicted_N) %>%
-      dplyr::filter(e3 > e1 + 0.50 & e3 > e2 + 0.50) %>%
-      tidyr::pivot_longer(cols = c(e1, e2, e3), names_to = "leaf", values_to = "mean_predicted_N")
-    
-    e3_higher_genotypes$condition <- paste0("e3_highest_", treatment, "_", stage)
-    
-    # Combine all the data frames
-    total_combined <- rbind(all_above_CI_details, all_below_CI_details, e3_lower_genotypes, e3_higher_genotypes)
-    
-    # Save the combined data for further analysis
-    write.csv(total_combined, paste0("Total_combined_", treatment, "_", stage, ".csv"), row.names = FALSE)
+    # Save the summary as a CSV file
+    write.csv(plant_summary, paste0("plant_summary_", treatment, "_", stage, ".csv"), row.names = FALSE)
   }
 }
+
+
+
+
 
 
 
